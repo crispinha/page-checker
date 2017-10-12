87f72a632ab6cc6328a0855b877be0e55eda34cd
Crispin Hitchings-Anstice <dvlpstrcrispin@users.noreply.github.com>
2017-10-12 06:09:36 +0000

    i keep on writing print as console.log

--- getter.py
+++ getter.py
@@ -1,13 +1,24 @@
 import requests
 import hashlib
 site = "http://youtube.com"
+initial_hash = ""
 
 
 def get_hash():
     rep = requests.get(site)
     bytes = rep.text.encode("utf-8")
-    print(hashlib.md5(bytes).hexdigest())
+    return hashlib.md5(bytes).hexdigest()
+
+
+def set_initial_hash():
+    # i know i shouldn't use globals
+    global initial_hash
+    if initial_hash:
+        print("This shouldn't be called with an existing hash")
+        return False
+    initial_hash = get_hash()
 
 
 if __name__ == "__main__":
-    get_hash()
+    set_initial_hash()
+    print(initial_hash)
