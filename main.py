92456107e7219cfb80f86adbc901a41199793c61
Crispin Hitchings-Anstice <dvlpstrcrispin@users.noreply.github.com>
2017-10-12 06:45:01 +0000

    and everything else

---
+++ main.py
@@ -0,0 +1,17 @@
+import getter
+import discord
+import asyncio
+import config
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+
+client.run(config.very_secret_key)
